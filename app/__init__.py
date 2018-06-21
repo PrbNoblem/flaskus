901915815f38989fab721dcf6138b89d7e842392
mevoki <hannes.joensson@gmail.com>
2018-06-21 10:48:00 +0200

    working on database stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+# Config is defined such that the object passed to app above
+# contains what is needed by SQLAlchemy to do its thing.
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# routes define the views, while models will define the structure of the database
+from app import routes, models
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+# Name of class used as __tablename__ default
+class User(db.Model):
+    __tablename__ = 'TestTable'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
